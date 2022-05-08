3ca84b05f6a8f6d33f9d0c71ed9e7144dd3d75f3
Anna Velarde <annavelardevilaseca@gmail.com>
2022-05-08 15:14:11 +0100

    dealing with addEventListener and filter

--- main.js
+++ main.js
@@ -28,3 +28,33 @@ let nameDay = date.toLocaleString(languages, { weekday: "long" });
 
 document.querySelector(".month").innerHTML = month;
 document.querySelector(".date").innerHTML = nameDay + ", " + dayNumber;
+
+//working on input. selecting element matches the css
+const taskInPut = document.querySelector(".task-input");
+// console.log(taskInPut);
+
+//Button. working on adding toDo. use method to allow selecting the first element that matches the css selectors
+const addtaskbtn = document.querySelector(".add-task-btn");
+// console.log(addtaskbtn);
+
+const toDos = [];
+
+addtaskbtn.addEventListener("click", (e) => {
+  if (taskInPut.value == "") {
+    const divs = document.querySelectorAll(".task-list").length;
+    document.querySelector(".number-of-task").innerHTML = divs + "Tasks";
+    console.log(taskInPut);
+  } else {
+    return "empty";
+  }
+  //   toDos.push(taskInPut.value);
+  console.log(taskInPut);
+});
+
+//filter
+let filter = document.querySelectorAll(".task-menu-container");
+//filter+btn
+//Second parameter is the function we want to call when the event occurs. Third parameter optional.
+// Filter.forEach((btn) => {
+//   btn.addEventListener("click", () => {});
+// });
